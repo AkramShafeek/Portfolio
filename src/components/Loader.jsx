03835b7b8bf52b287a61d4db86e7f1af05461d21
Akram <akramshafeek70@gmail.com>
2024-04-04 19:11:14 +0530

    Fixed loader issue

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,46 +6,58 @@ const Loader = () => {
 
   const order = [0, 1, 3, 2, 4, 6, 5, 7, 8];
   const [showable, setShowable] = useState([]);
-  const [color, setColor] = useState([]);  
+  const [color, setColor] = useState([]);
+  const [startColoring, setStartColoring] = useState(false);
+  const [removeLoader, setRemoveLoader] = useState(false);
 
-  const removeLoader = () => {
-    const interval = setInterval(() => {
-      setShowable(prevShowable => {
+
+  useEffect(() => {
+    if (showable.length === 0 || color.length === 0)
+      return;
+    const interval1 = setInterval(() => {
+      setShowable(prevShowable => {        
         if (prevShowable.length === 0) {
-          clearInterval(interval);                              
+          clearInterval(interval1);
           return prevShowable;
         }
+        console.log(prevShowable.filter((val, idx) => idx !== 0));
         return prevShowable.filter((val, idx) => idx !== 0);
       });
-    }, 200);
-  }
+    }, 90);
 
-  const startColoring = () => {
-    const interval = setInterval(() => {
+    return () => clearInterval(interval1);
+  }, [removeLoader]);
+
+  useEffect(() => {
+    if (showable.length === 0)
+      return;    
+    const interval2 = setInterval(() => {
       setColor(prevColor => {
         if (prevColor.length === order.length) {
-          clearInterval(interval);          
-          setTimeout(() => removeLoader(), 1000);          
+          clearInterval(interval2);
+          setTimeout(() => setRemoveLoader(true), 500);
           return prevColor;
-        }
+        }        
         return [...prevColor, order[prevColor.length]];
       });
-    }, 100);
-  }  
+    }, 90);
 
-  useEffect(() => {    
-    const interval = setInterval(() => {
+    return () => clearInterval(interval2);
+  }, [startColoring]);
+
+  useEffect(() => {
+    const interval3 = setInterval(() => {
       setShowable(prevShowable => {
         if (prevShowable.length === order.length) {
-          clearInterval(interval);                    
-          setTimeout(() => startColoring(), 1000);
+          clearInterval(interval3);
+          setTimeout(() => setStartColoring(true), 500);
           return prevShowable;
-        }
+        }        
         return [...prevShowable, order[prevShowable.length]];
       });
     }, 90);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(interval3);
   }, []);
 
 
@@ -56,7 +68,7 @@ const Loader = () => {
         row.map((col, j) =>
           <div key={i * mat[0].length + j}
             className={`w-1/4 aspect-square rounded-sm text-center ${showable.indexOf(i * mat[0].length + j) !== -1 ? 'opacity-1 scale-1' : 'opacity-0 scale-0'} transition-all ${color.indexOf(i * mat[0].length + j) !== -1 ? 'bg-emerald-500' : 'bg-slate-800'}`}
-            >            
+          >
           </div>
         ))}
     </div>
